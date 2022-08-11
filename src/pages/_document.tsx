00ab4661eb54f134e89afd98c018c8627ac66be9
João Melo <jopcmelo@gmail.com>
2022-08-11 15:52:46 -0400

    feat: define first version of the project

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="pt">
+        <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
+            rel="stylesheet"
+          />
+          {/* TODO uncomment the following line with the right icon */}
+          {/* <link rel="icon" href="/images/favicon.ico" /> */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    brand: {},
+  },
+  fonts: {
+    heading: "Roboto",
+    body: "Roboto",
+  },
+});

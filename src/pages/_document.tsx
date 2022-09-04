95432f34925d46273b96245fb5c916ddf8029d77
João Melo <jopcmelo@gmail.com>
2022-09-04 18:23:20 -0300

    feat: add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,24 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap"
             rel="stylesheet"
           />
-          <link rel="icon" href="/images/favicon.ico" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest"></link>
         </Head>
         <body>
           <Main />

bf813687aca50dc903424e6420154ad6b0760d63
João Melo <jopcmelo@gmail.com>
2022-10-26 17:18:35 -0300

    chore: remove tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,6 @@ import type { AppProps } from 'next/app'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 
-import '../styles/globals.css'
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>

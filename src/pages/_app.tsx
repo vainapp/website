386ec25416e244697c6f6882bac00cef34e36ae7
João Melo <jopcmelo@gmail.com>
2022-10-26 17:33:14 -0300

    feat: add Mantine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,28 @@
 import type { AppProps } from 'next/app'
+import { MantineProvider } from '@mantine/core'
+import Head from 'next/head'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
+      <Head>
+        <title>Grupo C</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{ colorScheme: 'light', fontFamily: 'Roboto, sans serif' }}
+      >
+        <Header />
+        <Component {...pageProps} />
+        <Footer />
+      </MantineProvider>
     </>
   )
 }
-
-export default MyApp

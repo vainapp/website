ee194898c5a99fe0ed55bfed05dcfe871c0440cf
João Melo <jopcmelo@gmail.com>
2022-10-26 19:48:34 -0300

    feat: add Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,70 @@
+import {
+  createStyles,
+  Container,
+  Group,
+  Anchor,
+  Image,
+  Box,
+} from '@mantine/core'
+
+const useStyles = createStyles((theme) => ({
+  footer: {
+    marginTop: 120,
+    borderTop: `1px solid ${theme.colors.gray[2]}`,
+  },
+
+  inner: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingTop: theme.spacing.xl,
+    paddingBottom: theme.spacing.xl,
+
+    [theme.fn.smallerThan('xs')]: {
+      flexDirection: 'column',
+    },
+  },
+
+  links: {
+    [theme.fn.smallerThan('xs')]: {
+      marginTop: theme.spacing.md,
+    },
+  },
+}))
+
+const links = [
+  {
+    link: '#',
+    label: 'Contato',
+  },
+  {
+    link: '#',
+    label: 'Privacidade',
+  },
+]
+
 export default function Footer() {
+  const { classes } = useStyles()
+  const items = links.map((link) => (
+    <Anchor<'a'>
+      color="dimmed"
+      key={link.label}
+      href={link.link}
+      onClick={(event) => event.preventDefault()}
+      size="sm"
+    >
+      {link.label}
+    </Anchor>
+  ))
+
   return (
-    <footer>
-      <p>Footer</p>
-    </footer>
+    <div className={classes.footer}>
+      <Container className={classes.inner}>
+        <Box sx={{ maxWidth: 40 }}>
+          <Image src="./logo.png" alt="Grupo C logo" />
+        </Box>
+        <Group className={classes.links}>{items}</Group>
+      </Container>
+    </div>
   )
 }

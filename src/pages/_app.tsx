05dcc238b228fac354b241713bb646cf5c8f8890
João Melo <jopcmelo@gmail.com>
2023-01-11 11:01:38 -0300

    feat: add Footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Header } from '../components/Hero'
+import { Footer } from '../components/Footer'
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
       <Header />
       <Component {...pageProps} />
+      <Footer />
     </>
   )
 }

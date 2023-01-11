063b4ec324e857b464222b6160c91046f18c1838
João Melo <jopcmelo@gmail.com>
2023-01-11 11:22:18 -0300

    feat: add Hero

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { Header } from '../components/Hero'
+import { Header } from '../components/Header'
 import { Footer } from '../components/Footer'
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { Hero } from '../components/Hero'
+
 export default function Home(): JSX.Element {
-  return <div>Homepage</div>
+  return <Hero />
 }

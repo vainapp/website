e7015ea127ff1d88df778c75fb379a485f2a9e04
João Melo <jopcmelo@gmail.com>
2023-01-11 12:15:34 -0300

    feat: add Pricing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
+import { CTAFreeTrial } from '../components/CTAFreeTrial'
 import { Features } from '../components/Features'
 import { Hero } from '../components/Hero'
+import { Pricing } from '../components/Pricing'
 
 export default function Home(): JSX.Element {
   return (
     <>
       <Hero />
       <Features />
+      <CTAFreeTrial />
+      <Pricing />
     </>
   )
 }

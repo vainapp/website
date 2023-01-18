412baa8cb0054df9e8c174a0f4523faeb55b900a
João Melo <jopcmelo@gmail.com>
2023-01-18 19:12:29 -0300

    feat: add project id to cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  projectId: 'ab7qr2',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
